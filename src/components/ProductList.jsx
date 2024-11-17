02b810f1895d51b5be4be6f9464ba734dca4e961
LuisVieira0701 <luisvieira@usf.edu>
2024-11-17 18:57:09 -0500

    T3-Create ProductList component to render each  ProductItem

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,13 @@
+import {App} from './App.jsx'
+
+function ProductList({products}) {
+    return (
+            <ul>
+                {products.map((product) => (
+                    <ProductItem key={product.id} product={product} />
+                ))}
+            </ul> 
+    )
+}
+
+export default ProductList
